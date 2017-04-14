0473dc7de3e8b1425e5fb8e42b42ca8fb3fde583
Dany BREGEON <dany@bregeon.net>
2017-04-14 19:24:12 +0200

    debut main

--- projetOCaml.ml
+++ projetOCaml.ml
@@ -1,22 +1,117 @@
+type formule =
+  Var of char
+| Vrai
+| Faux
+| Non of formule
+| Et of formule * formule
+| Ou of formule * formule
+| Imp of formule * formule
+| Ssi of formule * formule
+;;
 
+type interpretation == (char * bool) list
+;;
 
-type f = Non of f | Et of f*f | V of f*f | Imp of f*f | Eq of f*f | Zero | Un ;;
+type sequent == (formule list * formule list)
+;;
 
-let rec evaluer = fun f ->
-      match f with
-      | Zero -> Zero
-      | Un -> Un
-	  | Non Un -> Zero
-	  | Non Zero -> Un
-      | Non f -> let x = evaluer f in evaluer (Non(x))
-      | Et(_, Zero) -> Zero
-      | Et(Zero,_) -> Zero
-      | Et(Un, Un) -> Un
-      | Et(f,g) -> let x1 = evaluer f and x2 = evaluer g in evaluer (Et(x1, x2))
-	  | V(_, Un) -> Un
-      | V(Un,_) -> Un
-      | V(Zero, Zero) -> Zero
-      | V(f,g) -> let x1 = evaluer f and x2 = evaluer g in evaluer (V(x1, x2));;
-	  
-	  let formule = V(Non(Un),Et(Un,Zero));;
-	  evaluer formule;;
+exception Echec
+;;
+
+let ajoute p val (I:interpretation) =
+  try if (assoc p I)=val then I else raise Echec
+  with Not_found -> (p,val)::I
+;;
+
+let rec beth (I:interpretation) (seq:sequent) =
+  match seq with
+  ([],[])              -> I
+| ([],(Var p)::l)      -> beth (ajoute p false I) ([],l)
+| ([],Vrai::l)         -> raise Echec
+| ([],Faux::l)         -> beth I ([],l)
+| ([],(Non f)::l)      -> beth I ([f],l)
+| ([],(Et(f1,f2))::l)  -> begin try beth I ([],f1::l) 
+                          with Echec -> beth I ([],f2::l) end
+| ([],(Ou(f1,f2))::l)  -> beth I ([],f1::f2::l)
+| ([],(Imp(f1,f2))::l) -> beth I ([f1],f2::l)
+| ([],(Ssi(f1,f2))::l) -> begin try beth I ([f1],f2::l) 
+                          with Echec -> beth I ([f2],f1::l) end
+| ((Var p)::l,l2)      -> beth (ajoute p true I) (l,l2)
+| (Vrai::l,l2)         -> beth I (l,l2)
+| (Faux::l,l2)         -> raise Echec
+| ((Non f)::l,l2)      -> beth I (l,f::l2)
+| ((Et(f1,f2))::l,l2)  -> beth I (f1::f2::l,l2)
+| ((Ou(f1,f2))::l,l2)  -> begin try beth I (f1::l,l2)
+                          with Echec -> beth I (f2::l,l2) end
+| ((Imp(f1,f2))::l,l2) -> begin try beth I (l,f1::l2)
+                          with Echec -> beth I (f2::l,l2) end
+| ((Ssi(f1,f2))::l,l2) -> begin try beth I (f1::f2::l,l2) 
+                          with Echec -> beth I (l,f1::f2::l2) end
+;;
+
+let rec saute_blancs s =
+  match s with
+  [< '(` ` | `\t`) >] -> saute_blancs s
+| [< 'c >]            -> [< 'c; saute_blancs s>]
+| [< >]               -> s
+;;
+
+let rec connecteur s =
+  match s with
+  [< '`/`; '`\\` >]      -> (fun f1 f2 -> Et (f1,f2))
+| [< '`\\`; '`/` >]      -> (fun f1 f2 -> Ou (f1,f2))
+| [< '`=`; '`>` >]       -> (fun f1 f2 -> Imp (f1,f2))
+| [< '`<`; '`=`; '`>` >] -> (fun f1 f2 -> Ssi (f1,f2))
+;;
+
+let rec formule s =
+  match s with
+  [< '`(`; formule f1; connecteur c; formule f2; '`)` >] -> c f1 f2
+| [< '`~`; formule f >]                                  -> Non f
+| [< '`1` >]                                             -> Vrai
+| [< '`0` >]                                             -> Faux
+| [< '(`a`..`z` as p) >]                                 -> Var p
+;;
+
+let traduit s =
+  match (saute_blancs s) with
+  [< formule f; '`#` >] -> f
+;;
+
+let rec affiche (I:interpretation) =
+  match I with
+  (c,val)::I' -> printf__printf "%c=%d " c (if val then 1 else 0); affiche I'
+| []          -> print_string "\n"
+;;
+
+let main () =
+  print_string "Bonjour...\n";
+  print_string "Pour quitter tapez Ctrl-D en guise de formule\n";
+  begin try 
+    while true do 
+      print_string "Entrez une formule : ";
+      let s = read_line () in
+        begin try
+          let f = traduit (stream_of_string (s^"#")) in
+          try
+            let satisf = beth [] ([f],[]) in
+            try
+              let falsif = beth [] ([],[f]) in
+              print_string (s^" est contingente\n");
+              print_string ("Voici une interprétation satisfiante : ");
+              affiche satisf;
+              print_string ("Voici une interprétation falsifiante : ");
+              affiche falsif
+            with Echec -> print_string (s^" est une tautologie\n")
+          with Echec -> print_string (s^" est une antilogie\n")
+        with Parse_failure | Parse_error ->
+          print_string (s^" ne représente pas une formule correcte\n") end;
+      print_string "\n"
+    done
+  with End_of_file -> print_string "\nAu revoir..."
+    |  _           -> print_string "\nVous venez de tomber sur un bug... Bye"
+  end;
+  print_newline ()
+;;
+
+if sys__interactive then () else main ();;
---
+++ projetOCamlBerth.ml
@@ -0,0 +1,41 @@
+type formule =
+  Var of char
+| Vrai
+| Faux
+| Non of formule
+| Et of formule * formule
+| Ou of formule * formule
+| Imp of formule * formule
+| Ssi of formule * formule
+;;
+
+type interpretation == (char * bool) list;;
+
+let rec lireFormule() =
+	begin
+	print_string "Veuillez inserer votre formule:";
+	print_newline();
+	let formule = read_line () in
+	begin
+	print_newline(); print_string "Que voulez vous faire ? Satisfaire/Falsifier/Valider/Insatisfaire";
+	print_newline();
+	let choix = read_line () in
+	print_string (formule^"   "^choix);
+	print_newline();
+	end;
+	print_string "Voulez-vous recommencer ? si oui o sinon n";
+	print_newline();
+	let recommencer = read_line () in
+	if recommencer = "o" then lireFormule()
+	else print_string "Au revoir !";
+	end;;
+
+let main () =
+	begin
+	print_string "Bonjour !";
+	print_newline();
+	lireFormule()
+	end;;
+
+(*  lireFormule();;  *)
+main();;
